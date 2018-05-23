509293563ae86d8d6eaff82e69a2af2084e4f93c
monicacholico <monicachol@hotmail.com>
2018-05-23 15:39:01 -0500

    finished where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,12 +1,20 @@
 USE employees;
 SELECT first_name
 FROM employees
-WHERE first_name
-IN ('Irena', 'Vidya', 'Maya');
+WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
+AND gender = 'M';
 
 SELECT last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%E';
+
 
 SELECT *
 FROM employees
@@ -20,3 +28,15 @@ SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
 
+SELECT *
+FROM employees
+WHERE birth_date
+LIKE '%-12-25'
+AND( hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+);
+
+SELECT *
+FROM employees
+WHERE last_name
+LIKE '%q%'
+AND NOT LIKE '%qu%';
