a171af9e5d29d776a2fa37264e35e3760fd53683
monicacholico <monicachol@hotmail.com>
2018-05-23 14:19:51 -0500

    finished first part of where exercises

---
+++ where_exercises.sql
@@ -0,0 +1,22 @@
+USE employees;
+SELECT first_name
+FROM employees
+WHERE first_name
+IN ('Irena', 'Vidya', 'Maya');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
